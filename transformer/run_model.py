36b91aae194ef2a85ba5a41a1252c6a50062a142
jean-yoo <jeanyoo16@gmail.com>
2022-12-10 10:33:53 -0500

    inference

--- transformer/run_model.py
+++ transformer/run_model.py
@@ -94,17 +94,6 @@ def model_personal_inference(model, collator):
     print(answer_space[preds[0]])
     model.is_personal = False # set it back to false
 
-#model, collator, train_metrics, eval_metrics = train_model()
-text_tokenizer = AutoTokenizer.from_pretrained('roberta-base')
-feature_extractor = AutoFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
-# initialize components 
-collator = MultimodalCollator(text_tokenizer, feature_extractor)
-model = TransformerModel()
-checkpoint = torch.load('/Users/jean/Documents/CS1470/dl-final-project/transformer/transformer-checkpoints/runs/Dec05_22-02-26_jeans-mbp-2.devices.brown.edu/events.out.tfevents.1670295746.jeans-mbp-2.devices.brown.edu.28802.0')
-model.load_state_dict(checkpoint['model_state_dict'])
-epoch = checkpoint['epoch']
-loss = checkpoint['loss']
-
-model.eval()
+model, collator, train_metrics, eval_metrics = train_model()
 model_inference(model, collator)
 model_personal_inference(model, collator)
