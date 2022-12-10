c0b205c91c1316b6b17b208fb08e6aadb901b954
jean-yoo <jeanyoo16@gmail.com>
2022-12-10 15:49:37 -0500

    idk

--- transformer/run_model.py
+++ transformer/run_model.py
@@ -106,7 +106,7 @@ def model_personal_inference(model, collator):
     model.eval() 
     output = model(img_features = img_feat, text_features = text_feat, labels = labels)
     preds = output['logits'].argmax(axis = -1).cpu().numpy() 
-    print(answer_space[preds[0]])
+    print(answer_space[preds[i] for i in range(0, 8)])
     collator.is_personal = False # set it back to false
 
 #model, collator, train_metrics, eval_metrics = train_model()
